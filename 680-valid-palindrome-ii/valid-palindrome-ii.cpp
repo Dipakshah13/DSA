1aeedf302d4af2246cda833261cb8c24db52eae1
Dipakshah13 <dipakshah19624@gmail.com>
2026-09-16 19:23:55 +0530

    Time: 8 ms (22.13%) | Memory: 26.3 MB (40.16%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+
+     bool checkPalindrome(string s, int start,int end)
+    {
+
+             while(start <= end)
+             {
+                if(s[start] != s[end])
+                {
+                    return false;
+                }
+                else
+                {
+                    start++;
+                    end--;
+                }
+             }
+        
+        return true;
+     }
+    bool validPalindrome(string s) {
+    
+         int start =0;
+          int end = s.length() -1;
+        while(start <= end)
+        {
+            if(s[start] == s[end])
+            {
+                start++;
+                end--;
+            }
+            else
+            {
+                return checkPalindrome(s,start,end-1) || checkPalindrome(s,start+1,end);
+            }
+        }
+        return true;
+    }
+};
