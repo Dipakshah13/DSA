c979bb61717206d57a85abea1bc71b6a68fe6f65
Dipakshah13 <dipakshah19624@gmail.com>
2026-09-19 18:22:30 +0530

    Time: 14 ms (11.40%) | Memory: 13.5 MB (17.82%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        map<char,int> m ={ {'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}     
+        unordered_map<char,int> m ={ {'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}     
         };
 
         int i =0 , n = s.size();
